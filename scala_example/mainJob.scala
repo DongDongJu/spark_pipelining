4e85fb96452e5a4fd7d6bebc61d8b5b6cc1565dc
DongDongJu <commisori28@gmail.com>
2015-07-30 21:37:05 +0900

    fix

--- scala_example/mainJob.scala
+++ scala_example/mainJob.scala
@@ -4,12 +4,12 @@ import org.apache.spark._
 import org.apache.spark.SparkContext._
 object Hello {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("hello").setMaster("local")
+    val conf = new SparkConf().setAppName("hello").setMaster("yarn-cluster")
     val sc = new SparkContext(conf)
-    val i = 1
+    val i = 0
     val textLinesRDD = sc.textFile("hdfs://192.168.100.123:9000/user/hadoop/input/1")
     try {
-      for(i <- 1 to 10){
+      for(i <- 0 to 3){
       var outRDD = textLinesRDD
                     .flatMap(_.split("""\s+"""))
                     .map((_, 1))
