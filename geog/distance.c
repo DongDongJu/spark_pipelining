ed3fa473dfc3e23bb3dd35d2bae17678045c34d9
DongDongJu <commisori28@gmail.com>
2015-08-18 15:38:41 +0900

    distance calculator

---
+++ geog/distance.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define R 6371
+#define TO_RAD (3.1415926536 / 180)
+double dist(double th1, double ph1, double th2, double ph2)
+{
+        double dx, dy, dz;
+        ph1 -= ph2;
+        ph1 *= TO_RAD, th1 *= TO_RAD, th2 *= TO_RAD;
+
+        dz = sin(th1) - sin(th2);
+        dx = cos(ph1) * cos(th1) - cos(th2);
+        dy = sin(ph1) * cos(th1);
+        return asin(sqrt(dx * dx + dy * dy + dz * dz) / 2) * 2 * R;
+}
+
+int main(int argc, char* argv[])
+{
+    if(argc >= 5)
+    {
+        printf("Distance : %f\n",dist(atof(argv[1]),atof(argv[2]),atof(argv[3]),atof(argv[4])));
+    }
+    else
+    {
+        printf("input correct parameters\n");
+    }
+
+    return 0;
+}
+
